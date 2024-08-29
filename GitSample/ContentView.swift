df4de2310b16eacdbca6895c7ff6feb1ed3ac1a4
kemu358 <gooaritoshi48@gmail.com>
2024-08-29 12:51:23 +0900

    Initial Commit

---
+++ GitSample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GitSample
+//
+//  Created by Kensuke Aritoshi on 2024/08/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GitSample/GitSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitSampleApp.swift
+//  GitSample
+//
+//  Created by Kensuke Aritoshi on 2024/08/29.
+//
+
+import SwiftUI
+
+@main
+struct GitSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
